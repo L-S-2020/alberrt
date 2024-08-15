72ce105f588c6d7227920b5efed2c3606cec3bc1
L-S-2020 <leostewal@gmail.com>
2024-08-16 01:42:35 +0200

    changed the formatting of the json querries from [""] to [''] (because of the nest python interpreter)

--- ai.py
+++ ai.py
@@ -5,10 +5,10 @@ from slack_sdk.web import WebClient
 
 # get secrets from .env file
 load_dotenv()
-WORKERS_AI_TOKEN = os.environ["WORKERS_AI_TOKEN"]
-WORKERS_AI_ACCOUNT = os.environ["WORKERS_AI_ACCOUNT"]
+WORKERS_AI_TOKEN = os.environ['WORKERS_AI_TOKEN']
+WORKERS_AI_ACCOUNT = os.environ['WORKERS_AI_ACCOUNT']
 
-BOT_USER_ID = os.environ["BOT_USER_ID"]
+BOT_USER_ID = os.environ['BOT_USER_ID']
 
 # define the system prompt
 SYSTEM_PROMPT = f"You are Alberrt, the the good soul of leonard's personal slack channel #leonards-valley. Answer every question truthfully, do not make anything up. If you want to ping a user, use the format <@user_id>, for example <@U075VTXR7D4>. Your own user_id is {BOT_USER_ID} ."
@@ -18,7 +18,7 @@ API_BASE_URL = f"https://api.cloudflare.com/client/v4/accounts/{WORKERS_AI_ACCOU
 headers = {"Authorization": "Bearer " + WORKERS_AI_TOKEN}
 
 # initialize the slack web client
-client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
 # variable to cache user information (for less API calls)
 user_cache = {}
@@ -34,8 +34,8 @@ def welcome(user_id):
     # get user information
     if user_cache.get(user_id) is None:
         user = client.users_info(user=user_id)
-        user = user["user"]
-        user_info = {"real name": user["profile"]["real_name"], "username": user["profile"]["display_name"], "user_id": user["id"]}
+        user = user['user']
+        user_info = {"real name": user['profile']['real_name'], "username": user['profile']['display_name'], "user_id": user['id']}
         user_cache[user_id] = user_info
     else:
         user_info = user_cache[user_id]
@@ -45,46 +45,46 @@ def welcome(user_id):
         { "role": "user", "content": f"Welcome the user <@{user_id}>, say something about the channel and introduce yourself short. Userinformation: {str(user_info)}."}
     ];
     output = run("@cf/meta/llama-3.1-8b-instruct", inputs)
-    response = output["result"]["response"]
+    response = output['result']['response']
     # I'll make this fallback better later
     if f"<@{user_id}>" not in response:
         output = run("@cf/meta/llama-3.1-8b-instruct", inputs)
-        response = output["result"]["response"]
+        response = output['result']['response']
     return response
 
 # answer a message in a conversation
 def answer(history, channel, thread=False):
-    messages = history["messages"]
+    messages = history['messages']
     if not thread:
         messages.reverse()
     channel_info = client.conversations_info(channel=channel)
-    if channel_info["channel"]["is_im"]:
-        channel_info["channel"]["name"] = "Direct Message"
+    if channel_info['channel']['is_im']:
+        channel_info['channel']['name'] = "Direct Message"
     inputs = [
         {"role": "system", "content": SYSTEM_PROMPT},
-        {"role": "user", "content": f"Answer on the conversation, respond only with the reply. The conversation is in the channel name: {channel_info["channel"]["name"]} with the id: {channel_info["channel"]["id"]}."}
+        {"role": "user", "content": f"Answer on the conversation, respond only with the reply. The conversation is in the channel name: {channel_info['channel']['name']} with the id: {channel_info['channel']['id']}."}
     ];
     # add the previous messages to the inputs
     for message in messages:
         if message.get("user"):
-            if message["user"] == "U07HG4MRR6U":
-                inputs.append({"role": "assistant", "content": message["text"]})
+            if message['user'] == "U07HG4MRR6U":
+                inputs.append({"role": "assistant", "content": message['text']})
             else:
-                content = message["text"]
-                if user_cache.get(message["user"]) is None:
-                    user = client.users_info(user=message["user"])
-                    user = user["user"]
-                    user_info = {"real name": user["profile"]["real_name"], "username": user["profile"]["display_name"], "user_id": user["id"]}
-                    user_cache[message["user"]] = user_info
+                content = message['text']
+                if user_cache.get(message['user']) is None:
+                    user = client.users_info(user=message['user'])
+                    user = user['user']
+                    user_info = {"real name": user['profile']['real_name'], "username": user['profile']['display_name'], "user_id": user['id']}
+                    user_cache[message['user']] = user_info
                 else:
-                    user_info = user_cache[message["user"]]
+                    user_info = user_cache[message['user']]
                 content = "User: " + str(user_info) + " Message: " + content
                 inputs.append({"role": "user", "content": content})
         elif message.get("subtype") == "thread_broadcast":
-            inputs.append({"role": "assistant", "content": message["text"]})
+            inputs.append({"role": "assistant", "content": message['text']})
 
     output = run("@cf/meta/llama-3.1-8b-instruct", inputs)
-    response = output["result"]["response"]
+    response = output['result']['response']
     return response
 
 # test case to see if the welcome message works (random user id)
--- main.py
+++ main.py
@@ -10,12 +10,12 @@ logging.basicConfig(format='{asctime} - {levelname} - {message}', style='{', dat
 
 # get secrets from .env file
 load_dotenv()
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-SLACK_SOCKET_TOKEN = os.environ["SLACK_SOCKET_TOKEN"]
+SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+SLACK_SOCKET_TOKEN = os.environ['SLACK_SOCKET_TOKEN']
 
 # the id of my personal channel, idk if I should save it as environment variable
-PERSONAL_CHANNEL_ID = os.environ["PERSONAL_CHANNEL_ID"]
-BOT_USER_ID = os.environ["BOT_USER_ID"]
+PERSONAL_CHANNEL_ID = os.environ['PERSONAL_CHANNEL_ID']
+BOT_USER_ID = os.environ['BOT_USER_ID']
 
 # import slack sdk
 from slack_sdk.web import WebClient
@@ -39,54 +39,54 @@ def listener(client: SocketModeClient, req: SocketModeRequest):
         logging.info(f"Received an event: {req.payload['event']['type']}")
 
         # Welcome new user to my personal channel
-        if req.payload["event"]["type"] == "member_joined_channel" and req.payload["event"]["channel"] == PERSONAL_CHANNEL_ID:
-            answer_text = welcome(req.payload["event"]["user"])
-            client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text)
+        if req.payload['event']['type'] == "member_joined_channel" and req.payload['event']['channel'] == PERSONAL_CHANNEL_ID:
+            answer_text = welcome(req.payload['event']['user'])
+            client.web_client.chat_postMessage(channel=req.payload['event']['channel'], text=answer_text)
             logging.info(f"Sent welcome message to user {req.payload['event']['user']} in channel {req.payload['event']['channel']} with text: {answer_text}")
 
         # Answer to messages when mentioned
-        if req.payload["event"]["type"] == "app_mention":
-            logging.info(f"Was mentioned in channel {req.payload["event"]["channel"]} by {req.payload["event"]["user"]} with text: {req.payload["event"]["text"]}")
+        if req.payload['event']['type'] == "app_mention":
+            logging.info(f"Was mentioned in channel {req.payload['event']['channel']} by {req.payload['event']['user']} with text: {req.payload['event']['text']}")
 
             # if the message is in a thread, answer in the thread
-            if req.payload["event"].get("thread_ts"):
-                history = client.web_client.conversations_replies(channel=req.payload["event"]["channel"], ts=req.payload["event"]["thread_ts"])
-                answer_text = answer(history, req.payload["event"]["channel"], True)
-                client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text, thread_ts=req.payload["event"]["thread_ts"])
+            if req.payload['event'].get("thread_ts"):
+                history = client.web_client.conversations_replies(channel=req.payload['event']['channel'], ts=req.payload['event']['thread_ts'])
+                answer_text = answer(history, req.payload['event']['channel'], True)
+                client.web_client.chat_postMessage(channel=req.payload['event']['channel'], text=answer_text, thread_ts=req.payload['event']['thread_ts'])
                 logging.info(f"Answered in thread {req.payload['event']['thread_ts']} with text: {answer_text}")
 
             # if the message is not in a thread, answer in the thread of the message and send to the channel
             else:
-                history = client.web_client.conversations_history(channel=req.payload["event"]["channel"], latest=req.payload["event"]["ts"], limit=5, inclusive=True)
-                answer_text = answer(history, req.payload["event"]["channel"], False)
-                client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text, thread_ts=req.payload["event"]["ts"], reply_broadcast=True)
+                history = client.web_client.conversations_history(channel=req.payload['event']['channel'], latest=req.payload['event']['ts'], limit=5, inclusive=True)
+                answer_text = answer(history, req.payload['event']['channel'], False)
+                client.web_client.chat_postMessage(channel=req.payload['event']['channel'], text=answer_text, thread_ts=req.payload['event']['ts'], reply_broadcast=True)
                 logging.info(f"Answered with broadcast in thread {req.payload['event']['ts']} with text: {answer_text}")
 
         # Answer to direct messages
-        if req.payload["event"]["type"] == "message":
+        if req.payload['event']['type'] == "message":
             # check if message comes from the bot itself
             bot = False
-            if req.payload["event"].get("bot_id"):
+            if req.payload['event'].get("bot_id"):
                 bot = True
-            elif req.payload["event"].get("user") == BOT_USER_ID:
+            elif req.payload['event'].get("user") == BOT_USER_ID:
                 bot = True
 
             # check if it's a new direct message
-            if req.payload["event"]["channel_type"] == "im" and not bot and req.payload["event"].get("subtype") != "message_changed" and req.payload["event"].get("subtype") != "message_deleted":
+            if req.payload['event']['channel_type'] == "im" and not bot and req.payload['event'].get("subtype") != "message_changed" and req.payload['event'].get("subtype") != "message_deleted":
                 logging.info(f"Received a message in a direct message from {req.payload['event']['user']} with text: {req.payload['event']['text']}")
 
                 # if the message is in a thread, answer in the thread
-                if req.payload["event"].get("thread_ts"):
-                    history = client.web_client.conversations_replies(channel=req.payload["event"]["channel"], ts=req.payload["event"]["thread_ts"])
-                    answer_text = answer(history, req.payload["event"]["channel"], True)
-                    client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text, thread_ts=req.payload["event"]["thread_ts"])
+                if req.payload['event'].get("thread_ts"):
+                    history = client.web_client.conversations_replies(channel=req.payload['event']['channel'], ts=req.payload['event']['thread_ts'])
+                    answer_text = answer(history, req.payload['event']['channel'], True)
+                    client.web_client.chat_postMessage(channel=req.payload['event']['channel'], text=answer_text, thread_ts=req.payload['event']['thread_ts'])
                     logging.info(f"Answered in thread {req.payload['event']['thread_ts']} with text: {answer_text}")
 
                 # else answer in the thread of the message and send to the channel
                 else:
-                    history = client.web_client.conversations_history(channel=req.payload["event"]["channel"], latest=req.payload["event"]["ts"], limit=5, inclusive=True)
-                    answer_text = answer(history, req.payload["event"]["channel"], False)
-                    client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text, thread_ts=req.payload["event"]["ts"], reply_broadcast=True)
+                    history = client.web_client.conversations_history(channel=req.payload['event']['channel'], latest=req.payload['event']['ts'], limit=5, inclusive=True)
+                    answer_text = answer(history, req.payload['event']['channel'], False)
+                    client.web_client.chat_postMessage(channel=req.payload['event']['channel'], text=answer_text, thread_ts=req.payload['event']['ts'], reply_broadcast=True)
                     logging.info(f"Answered with broadcast in thread {req.payload['event']['ts']} with text: {answer_text}")
 
 # add listener to client
