20e13a3eaf27e116c7998a0afddaf26133eba685
L-S-2020 <leostewal@gmail.com>
2024-08-15 19:10:16 +0200

    Added the ability to respond to direct messages (without mention), added logging and documented my code

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# Alberrt, an ai slack bot for my personal channel
+# Alberrt, an AI based slack bot for my personal channel
 
 ![alberrt](https://raw.githubusercontent.com/L-S-2020/alberrt/master/alberrt.jpg)
+
+## What is Alberrt?
+Alberrt is a slack bot that I created for my personal slack channel. Currently, it can do the following:
+- Welcome new users
+- Respond to mentions
+- Respond to direct messages
+- mention a user
+
+## Underlining Technologies
+- Python
+- Slack SDK
+- Cloudflare Workers AI
+- The llama-3.1-8b-instruct model
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # import secrets
 import os
 from dotenv import load_dotenv
-# import the ai functions
+# import the ai functions from ai.py
 from ai import welcome, answer
 
+# set up logging
+import logging
+logging.basicConfig(format='{asctime} - {levelname} - {message}', style='{', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+
+# get secrets from .env file
 load_dotenv()
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 SLACK_SOCKET_TOKEN = os.environ["SLACK_SOCKET_TOKEN"]
@@ -25,43 +30,68 @@ client = SocketModeClient(
 
 # define event listener
 def listener(client: SocketModeClient, req: SocketModeRequest):
-    print(req.payload)
-    print(req.type)
     # react on events (messages, mentions, etc.)
     if req.type == "events_api":
         # Acknowledge the request anyway
         response = SocketModeResponse(envelope_id=req.envelope_id)
         client.send_socket_mode_response(response)
+        logging.info(f"Received an event: {req.payload['event']['type']}")
 
         # Welcome new user to my personal channel
         if req.payload["event"]["type"] == "member_joined_channel" and req.payload["event"]["channel"] == PERSONAL_CHANNEL_ID:
             answer_text = welcome(req.payload["event"]["user"])
-            print(answer_text)
             client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text)
+            logging.info(f"Sent welcome message to user {req.payload['event']['user']} in channel {req.payload['event']['channel']} with text: {answer_text}")
 
         # Answer to messages when mentioned
         if req.payload["event"]["type"] == "app_mention":
+            logging.info(f"Was mentioned in channel {req.payload["event"]["channel"]} by {req.payload["event"]["user"]} with text: {req.payload["event"]["text"]}")
 
             # if the message is in a thread, answer in the thread
             if req.payload["event"].get("thread_ts"):
                 history = client.web_client.conversations_replies(channel=req.payload["event"]["channel"], ts=req.payload["event"]["thread_ts"])
-                print(history)
                 answer_text = answer(history, req.payload["event"]["channel"], True)
                 client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text, thread_ts=req.payload["event"]["thread_ts"])
+                logging.info(f"Answered in thread {req.payload['event']['thread_ts']} with text: {answer_text}")
+
             # if the message is not in a thread, answer in the thread of the message and send to the channel
             else:
                 history = client.web_client.conversations_history(channel=req.payload["event"]["channel"], latest=req.payload["event"]["ts"], limit=5, inclusive=True)
-                print(history)
                 answer_text = answer(history, req.payload["event"]["channel"], False)
-                client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text,)
+                client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text, thread_ts=req.payload["event"]["ts"], reply_broadcast=True)
+                logging.info(f"Answered with broadcast in thread {req.payload['event']['ts']} with text: {answer_text}")
 
+        # Answer to direct messages
         if req.payload["event"]["type"] == "message":
-            if req.payload["event"]["text"] == "test":
-                client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text="test successful")
+            # check if message comes from the bot itself
+            bot = False
+            if req.payload["event"].get("bot_id"):
+                bot = True
+            elif req.payload["event"].get("user") == "U07HG4MRR6U":
+                bot = True
+
+            # check if it's a new direct message
+            if req.payload["event"]["channel_type"] == "im" and not bot and req.payload["event"].get("subtype") != "message_changed" and req.payload["event"].get("subtype") != "message_deleted":
+                logging.info(f"Received a message in a direct message from {req.payload['event']['user']} with text: {req.payload['event']['text']}")
+
+                # if the message is in a thread, answer in the thread
+                if req.payload["event"].get("thread_ts"):
+                    history = client.web_client.conversations_replies(channel=req.payload["event"]["channel"], ts=req.payload["event"]["thread_ts"])
+                    answer_text = answer(history, req.payload["event"]["channel"], True)
+                    client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text, thread_ts=req.payload["event"]["thread_ts"])
+                    logging.info(f"Answered in thread {req.payload['event']['thread_ts']} with text: {answer_text}")
+
+                # else answer in the thread of the message and send to the channel
+                else:
+                    history = client.web_client.conversations_history(channel=req.payload["event"]["channel"], latest=req.payload["event"]["ts"], limit=5, inclusive=True)
+                    answer_text = answer(history, req.payload["event"]["channel"], False)
+                    client.web_client.chat_postMessage(channel=req.payload["event"]["channel"], text=answer_text, thread_ts=req.payload["event"]["ts"], reply_broadcast=True)
+                    logging.info(f"Answered with broadcast in thread {req.payload['event']['ts']} with text: {answer_text}")
 
 # add listener to client
 client.socket_mode_request_listeners.append(listener)
 client.connect()
+logging.info("Connected to Slack Socket Mode")
 
 # keep running
 from threading import Event
